3d93d0bf206ceb04745e44a7eece96293a1d49b4
willamygarcia <willamy.t.i@gmail.com>
2022-07-30 23:05:36 -0300

    Created class´s Bootcamp and Dev

--- src/br/com/dio/desafio/domain/Bootcamp.java
+++ src/br/com/dio/desafio/domain/Bootcamp.java
@@ -1,5 +1,104 @@
 package br.com.dio.desafio.domain;
 
-public class Bootcamp {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	private String nome;
+	
+	private String descricao;
+	
+	private final LocalDate dataInicial = LocalDate.now();
+	
+	private final LocalDate datafinal = dataInicial.plusDays(45);
+	
+	private Set<Dev> devs = new HashSet<>();
+	
+	private Set<Conteudo> conteudos = new LinkedHashSet<>();
+	
+	public Bootcamp() {
+	}
+
+	public Bootcamp(Integer id, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Set<Dev> getDevs() {
+		return devs;
+	}
+
+	public void setDevs(Set<Dev> devs) {
+		this.devs = devs;
+	}
+
+	public Set<Conteudo> getConteudos() {
+		return conteudos;
+	}
+
+	public void setConteudos(Set<Conteudo> conteudos) {
+		this.conteudos = conteudos;
+	}
+
+	public LocalDate getDataInicial() {
+		return dataInicial;
+	}
+
+	public LocalDate getDatafinal() {
+		return datafinal;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bootcamp other = (Bootcamp) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 
 }
--- src/br/com/dio/desafio/domain/Dev.java
+++ src/br/com/dio/desafio/domain/Dev.java
@@ -1,5 +1,86 @@
 package br.com.dio.desafio.domain;
 
-public class Dev {
+import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+public class Dev implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	private String nome;
+	
+	private Set<Conteudo> mentorias = new LinkedHashSet<>();
+	
+	private Set<Conteudo> cursos = new LinkedHashSet<>();
+
+	public Dev() {
+	}
+
+	public Dev(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public void inscrincao() {
+		
+	}
+	
+	public void calculoTotalXP() {}
+	
+	public Conteudo exibirCursos() {return null;}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Conteudo> getMentorias() {
+		return mentorias;
+	}
+
+	public void setMentorias(Set<Conteudo> mentorias) {
+		this.mentorias = mentorias;
+	}
+
+	public Set<Conteudo> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(Set<Conteudo> cursos) {
+		this.cursos = cursos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dev other = (Dev) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 }
